b2b6a9abd4e14bd6c8f384aad269f8a5276f7bbc
milacerveira <milascerveira@gmail.com>
2021-01-09 04:59:39 +0000

    updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   
   const titleInput = document.querySelector('#title');
-  textInput.addEventListener('input', handleTitle);
+  titleInput.addEventListener('submit', handleTitle);
 
-  const authorInput = document.querySelector('#author');
-  textInput.addEventListener('input', handleAuthor);
+  //const authorInput = document.querySelector('#author');
+  //authorInput.addEventListener('input', handleAuthor);
 
-  // const selectOption = document.querySelector('.form-item');
-  // selectOption.addEventListener('change', handleSelect);
+  //const selectOption = document.querySelector('#category');
+  //selectOption.addEventListener('change', handleSelect);
 
-  // const button = document.querySelector('#button');
-  // button.addEventListener('click', handleClick);
+  //const button = document.querySelector('#button');
+  //button.addEventListener('click', handleClick);
 
 })
 
@@ -27,12 +27,48 @@ const handleAuthor = (event) => {
 }
 
 
-// const handleSelect = (event) => {
-//   event.preventDefault();
-//   const resultParagraph = document.querySelector('#category');
-//   resultParagraph.textContent = `Selected category: ${event.target.value}`;
-// }
+const handleSelect = (event) => {
+  event.preventDefault();
+  const resultParagraph = document.querySelector('#reading-list');
+  resultParagraph.textContent = `Selected category: ${event.target.value}`;
+}
 
-// const handleClick = function() {
-//   const resultParagraph = document.querySelector('#button')
-// }
+const handleClick = function(event) {
+  // const resultParagraph = document.querySelector('#button');
+  const newListItem = document.createElement('li');
+
+  newListItem.textContent = authorInput;
+  newListItem.classList.add('authorInput');
+  const list = document.querySelector('ul');
+  list.appendChild(newListItem);
+
+  newListItem2.textContent = titleInput;
+  newListItem2.classList.add('titleInput');
+  // const list = document.querySelector('ul');
+  list.appendChild(newListItem2);
+
+  newListItem3.textContent = selectOption;
+  newListItem3.classList.add('selectOption');
+  // const list = document.querySelector('ul');
+  list.appendChild(newListItem3);
+
+}
+
+// // 1. Create the element
+// const newListItem = document.createElement('li');
+
+// // 2. Modify the element
+// newListItem.textContent = "Purple";
+// newListItem.classList.add('purple');
+
+// // 3. Append the element to the DOM
+// const list = document.querySelector('ul');
+// list.appendChild(newListItem);
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const resultParagraph = document.querySelector('#form-result');
+  resultParagraph.textContent = `Your name: 
+  ${event.target.first_name.value}
+  ${event.target.last_name.value}`
+}
